c631b05b33064f7031136a4d071b61c5b851b249
ZeenatFirdosh <zeenatzfq052@gmail.com>
2023-10-08 17:49:06 +0530

    exact removed from route/

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -81,7 +81,7 @@ function App() {
       )}
 
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route path="/" component={Home} />
         <Route exact path="/product/:id" component={ProductDetails} />
         <Route exact path="/products" component={Products} />
         <Route path="/products/:keyword" component={Products} />
