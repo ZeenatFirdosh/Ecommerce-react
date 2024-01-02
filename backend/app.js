320789eaf971b71dbfa2abc1c89066a72e925c0c
Zeenat Firdosh Quadri <100707152+ZeenatFirdosh@users.noreply.github.com>
2024-01-02 10:38:25 +0000

    cors

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 var cors = require('cors');
 const app = express();
 app.use(cors({ origin: "*", credentials: true }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
